d918a399d3705643777061c34e83626a45fa2803
ligebit <31742363+ligebit@users.noreply.github.com>
2023-11-30 17:40:49 +0200

    upd

--- utils.js
+++ utils.js
@@ -9,7 +9,7 @@ function sleep(ms) {
 
 function getConfig(file) {
     return new Promise((res, rej) => {
-        readFile(__dirname + '\\' +  file, (err, data) => {
+        readFile(__dirname + '\/' +  file, (err, data) => {
             if (err) return rej(err);
 
             return res(JSON.parse(data.toString()));
@@ -19,7 +19,7 @@ function getConfig(file) {
 
 function saveConfig(obj, file) {
     return new Promise((res, rej) => {
-        writeFile(__dirname + '\\' + file, JSON.stringify(obj), (err) => {
+        writeFile(__dirname + '\/' + file, JSON.stringify(obj), (err) => {
             if (err) return rej(err);
 
             return res();
